42935303a5583793339c3531551428669143078d
Christopher Villamarín <xeland314@christopher-pc.xeland314>
2022-11-20 23:45:59 -0500

    feat: add background color to text color

--- include/Color.h
+++ include/Color.h
@@ -3,9 +3,10 @@
 
 typedef struct {
     const void * class;
-    char * color;
-    char * mode;
-} ColorType;
+    unsigned int attr;  // mode
+    unsigned int fg; 	// foreground, text color
+    unsigned int bg;    // background color
+} ColorType;  
 
 extern const void * Color;
 
@@ -13,29 +14,30 @@ void setColor(const void * _self);
 void resetColor();
 
 /* Color definitions: */
-#define START_COLOR     "\033["
 #define NORMAL_STDOUT   "\033[0m"
 
-// Option colors:
-#define NONE    "0m" 
-#define BLACK   "30m"
-#define RED     "31m"
-#define GREEN   "32m"
-#define YELLOW  "33m"
-#define BLUE    "34m"
-#define MAGENT  "35m"
-#define CYAN    "36m"
-#define WHITE   "37m"
+// Option colors {fg} and {bg}:
+#define BLACK   0
+#define RED     1
+#define GREEN   2
+#define YELLOW  3
+#define BLUE    4
+#define MAGENTA 5
+#define CYAN    6
+#define WHITE   7
 
-// Mode colors:
-#define NO_MODE     ""
-#define NORMAL      "0;"
-#define BOLD        "1;"
-#define DARK        "2;"
-#define ITALIC      "3;"
-#define UNDERLINE   "4;"
-#define BLINKING    "5;"
-#define HIGHLIGHTED "7;"
-#define CROSSED_OUT "9;"
+// Mode colors {attr}
+#define NORMAL      0
+#define BOLD        1
+#define BRIGHT      1
+#define DARK        2
+#define DIM         2
+#define ITALIC      3
+#define UNDERLINE   4
+#define BLINK       5
+#define HIGHLIGHTED 7
+#define REVERSED    7
+#define HIDDEN 	    8
+#define CROSSED_OUT 9
 
 #endif // !_COLOR_H_
--- tests/test02.c
+++ tests/test02.c
@@ -5,8 +5,9 @@
 
 int main()
 {
-    void * blue = new(Color, BLUE, DARK), * blue2 = clone(blue);
-    void * red = new(Color, RED, HIGHLIGHTED);
+    void * blue = new(Color, NORMAL, BLUE, WHITE);
+    void * blue2 = clone(blue);
+    void * red = new(Color, NORMAL, RED, YELLOW);
     
     printf("sizeOf(a) == %lu\n", sizeOf(blue));
     
@@ -19,11 +20,8 @@ int main()
     if (blue == blue2)
         puts("clone?");
 
-    setColor(blue);
-    puts("Texto en azul");
-    setColor(red);
-    puts("Texto en rojo");
-    resetColor();
+    printf("%sTexto en azul%s\n", toString(blue), NORMAL_STDOUT);
+    printf("%sTexto en rojo%s\n", toString(red), NORMAL_STDOUT);
     puts("Texto normal");
 
     delete(blue), delete(blue2), delete(red);
